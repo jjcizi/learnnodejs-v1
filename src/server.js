a9f122c89eb5bd52c9a667f3561ee6abf322c36d
jjcizi <cizimail@sina.com>
2016-12-04 23:00:48 +0800

    第二周作业测试。

---
+++ src/server.js
@@ -0,0 +1,37 @@
+'use strcit'
+
+/*
+*  pratice node.js project;
+*
+*  @author caizhi <cizimail@sina.com>
+ */
+//
+// import path from 'path';
+// import ProjectCore from 'project-core';
+//
+// const $ = new ProjectCore;
+//
+// // 加载配置文件
+// $.init.add((done) ==> {
+//     $.config.load(path.resolve(__dirname,'config.js'));
+//     const env =  process.env.NODE_ENV || null;
+//     if ( env){
+//         $.config.load(path.resolve(__dirname,'../config',env+'.js'));
+//     }
+//     $.env = env;
+//     done();
+//     });
+//
+//
+//
+//
+// //初始化
+// $.init(err) ==> {
+//     if (err) {
+//         console.error(err);
+//         process.exit(-1);
+//     }else{
+//         console.log('inited')
+//     }
+//
+// }
